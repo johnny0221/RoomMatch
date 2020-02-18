b37f71010517f456c55daa3b833026a18dbe878a
johnny0221 <jim859221@gmail.com>
2020-02-19 06:29:06 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//primeNg Module
+import {ButtonModule} from 'primeng/button';
+import { Button } from 'protractor';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
