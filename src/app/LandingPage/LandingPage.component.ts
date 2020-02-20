6422cbe06bc341388f7eb1230e6a1d8baecff4e1
johnny0221 <jim859221@gmail.com>
2020-02-20 10:23:28 +0800

    landingPage 1st version

---
+++ src/app/LandingPage/LandingPage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-LandingPage',
+  templateUrl: './LandingPage.component.html',
+  styleUrls: ['./LandingPage.component.scss']
+})
+export class LandingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// primeNg Module
+import {ButtonModule} from 'primeng/button';
 
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-//primeNg Module
-import {ButtonModule} from 'primeng/button';
-import { Button } from 'protractor';
+import { LandingPageComponent } from './LandingPage/LandingPage.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ButtonModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      LandingPageComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      ButtonModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
